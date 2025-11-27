f928c2d34ca305ec8e7141e35ed6ecb8a388978c
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2025-11-27 23:35:35 +0900

    feat: add common layout component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,21 @@
-import {
-  createBrowserRouter,
-  Link,
-  Outlet,
-  RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { AppLayout } from './components/AppLayout';
 
 const router = createBrowserRouter([
   {
-    element: <Outlet />,
+    element: (
+      <AppLayout>
+        <Outlet />
+      </AppLayout>
+    ),
     children: [
       {
         path: '/',
-        element: (
-          <div>
-            <div>タスク一覧</div>
-            <Link to="trash">ゴミ箱へ</Link>
-          </div>
-        ),
+        element: <div>タスク一覧</div>,
       },
       {
         path: '/trash',
-        element: (
-          <div>
-            <div>ゴミ箱</div>
-            <Link to="/">タスク一覧</Link>
-          </div>
-        ),
+        element: <div>ゴミ箱</div>,
       },
     ],
   },
