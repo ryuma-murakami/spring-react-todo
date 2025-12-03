91558cdc2bcefd756c1a2b42c237175d1dbc82cd
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2025-12-04 00:10:11 +0900

    feat: add trash page

--- frontend/src/hooks/useTasks.ts
+++ frontend/src/hooks/useTasks.ts
@@ -17,17 +17,29 @@ export function useTasks() {
     });
   };
 
-  const updateTask = (id: string, update: Partial<Task>) => {
+  const updateTask = (id: string, update: Partial<Task>) =>
     setTaskList(prevTaskList =>
       prevTaskList.map(task =>
         task.id === id ? { ...task, ...update } : task,
       ),
     );
-  };
+
+  const trashedTaskList = taskList.filter(({ status }) => status === 'trashed');
+
+  const deleteTask = (id: string) =>
+    setTaskList(prevTaskList => prevTaskList.filter(task => task.id !== id));
+
+  const deleteAllTrashedTasks = () =>
+    setTaskList(prevTaskList =>
+      prevTaskList.filter(({ status }) => status !== 'trashed'),
+    );
 
   return {
     activeTaskList,
     createTask,
     updateTask,
+    trashedTaskList,
+    deleteTask,
+    deleteAllTrashedTasks,
   };
 }
